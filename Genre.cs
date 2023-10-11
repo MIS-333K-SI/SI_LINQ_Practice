431de5a20360541276ca8642f45c7c438eb007dd
Anjali Bathra <anjalibathra@utexas.edu>
2023-10-11 16:33:27 -0500

    Initial commit

---
+++ Genre.cs
@@ -0,0 +1,15 @@
+﻿public class Genre
+    {
+        public Int32 GenreID { get; set; }
+
+        [Display(Name = "Genre")]
+        public String GenreName { get; set; }
+
+        //navigational property for books - a single genre will be associated with many books
+        public List<Book> Books { get; set; }
+
+        public Genre()
+        {
+            Books ??= new List<Book>();
+        }
+    }
---
+++ SearchViewModel.cs
@@ -0,0 +1,27 @@
+﻿    public enum PriceSearchType { GreaterThan, LessThan }
+	
+	public class SearchViewModel
+	{
+        [Display(Name = "Search by Title:")]
+        public String SearchName { get; set; }
+
+        [Display(Name = "Search by Description:")]
+        public String SearchDescription { get; set; }
+
+        [Display(Name = "Search by Format:")]
+        public Format? SelectedFormat { get; set; }
+
+        [Display(Name = "Search by Genre:")]
+        public Int32 SelectedGenreID { get; set; }
+
+        [Display(Name = "Search by Price:")]
+        public Decimal? SearchPrice { get; set; }
+
+        [Display(Name = "Type of Search:")]
+        public PriceSearchType SelectedType { get; set; } 
+
+        [Display(Name = "Search by Published Date:")]
+        [DataType(DataType.Date)]
+        public DateTime? SelectedDate { get; set; }
+    }
+
